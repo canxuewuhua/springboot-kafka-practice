c43bfea2cc1009335170dfacce3d00815d29d7dd
zhuyongqiang <981162250@qq.com>
2021-07-26 00:05:18 +0800

    完善完整的kafka测试逻辑，下一步安装工具eagle

---
+++ src/main/java/com/lifefamily/config/KafkaInitialConfiguration.java
@@ -0,0 +1,26 @@
+package com.lifefamily.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 初始化topic
+ */
+
+//@Configuration
+public class KafkaInitialConfiguration {
+
+    // 创建一个名为kafka20210725test的Topic并设置分区数为8，分区副本数为2
+    @Bean
+    public NewTopic initialTopic() {
+        return new NewTopic("kafka20210725test",8, (short) 2 );
+    }
+
+    // 如果要修改分区数，只需修改配置值重启项目即可
+    // 修改分区数并不会导致数据的丢失，但是分区数只能增大不能减小
+    @Bean
+    public NewTopic updateTopic() {
+        return new NewTopic("kafka20210725test",10, (short) 2 );
+    }
+}
