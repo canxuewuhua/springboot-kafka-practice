237799a7b0947c32bf038131876a4ece7c3bb61a
zhuyongqiang <981162250@qq.com>
2021-07-25 19:15:25 +0800

    第一次提交kafka

---
+++ src/main/java/com/lifefamily/LifeFamilyApplication.java
@@ -0,0 +1,12 @@
+package com.lifefamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeFamilyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeFamilyApplication.class, args);
+    }
+}
